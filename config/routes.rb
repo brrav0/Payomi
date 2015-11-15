98b8df067271f995ec7e2fddb3798392dc2a4161
ratinahirana <ratinahirana@gmail.com>
2015-11-15 13:20:29 +0100

    Solved: the button to delete did not work. Changed the method button_to by link_to. Don't know why, it seems that rails framework is building the DELETE method and when the object is nested, this method does not work.

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'bankaccounts#index'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
