fc3fc24f87cbc4a6a9d9af6a26f4b50bc4def114
brrav0 <brrav0@yahoo.com>
2015-12-08 12:23:23 +0100

    sendgrid for powerful retreat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host ='salty-forest-4286.herokuapp.com'
+  #host ='salty-forest-4286.herokuapp.com'
+  host ='powerful-retreat-8913.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
