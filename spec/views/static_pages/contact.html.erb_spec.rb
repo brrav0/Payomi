68d5755738d40d7b8029fa369fa625f9fa4a6f09
Bastien <brrav0@yahoo.com>
2016-01-02 18:06:29 +0100

    first tests with Rspec

---
+++ spec/views/static_pages/contact.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
+    it 'should display the home page' do
+      get :contact
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+  end
+end
