851e72086f87dd582609e7b3514e031fdd9bae96
Bastien <brrav0@yahoo.com>
2016-01-02 17:44:12 +0100

    installation of testing tools (rspec, factory girls, capybara)

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'activeadmin', '~> 1.0.0.pre1'
 
 # Carrier Wave to upload file
 gem 'carrierwave', '~> 0.9'
-gem "fog", "~> 1.35.0"
+gem 'fog', "~> 1.35.0"
 gem 'net-ssh'
 gem 'aws-sdk'
 
@@ -65,7 +65,11 @@ group :development, :test do
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'rspec','~> 3.0'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'database_cleaner'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,29 +1,28 @@
  require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-#to be reviseted step by step with other test email
-#  test "account_activation" do
-#    user = users(:michael)
-#    user.activation_token = User.new_token
-#    mail = UserMailer.account_activation(user)
-#    assert_equal "Account activation", mail.subject
-#    assert_equal [user.email], mail.to
-#    assert_equal ["from@example.com"], mail.from
-#    assert_match user.name,               mail.body.encoded
-#    assert_match user.activation_token,   mail.body.encoded
-#    assert_match CGI::escape(user.email), mail.body.encoded
-#  end
+  test "account_activation" do
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
+    #assert_equal "Activation de votre compte", mail.subject
+    #assert_equal [user.email], mail.to
+    #assert_equal ["pasderesponse@circularisation.com"], mail.from
+    #assert_match user.name,               mail.body.encoded
+    #assert_match user.activation_token,   mail.body.encoded
+    #assert_match CGI::escape(user.email), mail.body.encoded
+  end
 
-#    test "password_reset" do
-#    user = users(:michael)
-#    user.reset_token = User.new_token
-#    mail = UserMailer.password_reset(user)
-#    assert_equal "Password reset", mail.subject
-#    assert_equal [user.email], mail.to
-#    assert_equal ["from@example.com"], mail.from
-#    assert_match user.reset_token,        mail.body.encoded
-#    assert_match CGI::escape(user.email), mail.body.encoded
-#  end
+    test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    #assert_equal "Ré-initialisation de votre mot de passe", mail.subject
+    #assert_equal [user.email], mail.to
+    #assert_equal ["pasdereponse@circularisation.com"], mail.from
+    #assert_match user.reset_token,        mail.body.encoded
+    #assert_match CGI::escape(user.email), mail.body.encoded
+  end
 
 
 end
